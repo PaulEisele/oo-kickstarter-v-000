cbf5d3a44667cf701c44acaaa066594c84aacf50
paulheisele@gmail.com <paulheisele@gmail.com>
2018-07-04 14:36:31 +0000

    automatically backed up by learn

---
+++ lib/backer.rb
@@ -0,0 +1,24 @@
+class Backer
+  attr_accessor :backed_projects, :name
+
+  @@all = []
+
+  def initialize
+    @name = name
+    back_project = []
+    @@all = @name
+  end
+
+
+  def self.all
+    @@all
+  end
+
+
+  def back_project(project)
+    back_projects << project
+    project.add_backer(self)
+  end
+
+
+end
